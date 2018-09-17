645f8f23cce6fcaa0df47fd7c4e58e471f5f4bc5
matt6gsr <matt6gsr@gmail.com>
2018-09-17 08:07:50 +0100

    config edited

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 const config = {
-  development: 'mongodb://localhost:27017/northcoders-news',
-  test: 'mongodb://localhost:27017/northcoders-news-test',
-  production: 'mongodb://matt:abc123@ds159812.mlab.com:59812/ncnews'
+  development: { DB_URL: 'mongodb://localhost:27017/northcoders-news' },
+  test: { DB_URL: 'mongodb://localhost:27017/northcoders-news-test' },
+  production: { DB_URL: 'mongodb://matt:abc123@ds159812.mlab.com:59812/ncnews' }
 };
 
 module.exports = config[NODE_ENV];
