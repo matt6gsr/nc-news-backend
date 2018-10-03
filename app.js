b65bbb52f2e83ea8be937ce0493598b277b510b5
matt6gsr <matt6gsr@gmail.com>
2018-10-03 15:03:34 +0100

    imported cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const apiRouter = require('./routes/api-router');
 const {
